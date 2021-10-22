c8a38c8c102042cceceb22f9c467bc45c450c736
José David Baena <josedab@github.com>
2021-10-21 22:29:51 -0700

    Base setup with a controller

---
+++ src/main/java/com/josedab/springbootcodespaces/SpringBootCodespacesApplication.java
@@ -0,0 +1,13 @@
+package com.josedab.springbootcodespaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodespacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCodespacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josedab/springbootcodespaces/SpringBootCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josedab.springbootcodespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
